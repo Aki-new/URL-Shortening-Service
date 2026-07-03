f343f66c04711fa08299156f1d62cd7e8954d7f4
Aki-new <morillomiguel2707@gmail.com>
2026-07-03 00:09:01 -0400

    feature: connecting the controller and the model and add function for generate short code

--- controller.py
+++ controller.py
@@ -1,96 +1,92 @@
 from pydantic import HttpUrl
 from fastapi import HTTPException, status
 from datetime import datetime
+from model import Database
 
 class URLController:
+    def __init__(self):
+        self.db = Database()
+
     def create_shorten_url(self, url: HttpUrl):
-        if url == "https://example.com/shortened-url":
-            return HTTPException(
+        url = str(url)  # Convert HttpUrl to string
+
+        # Check if URL exists
+        if self.db.url_exists(url):
+            raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
                 detail="URL is already registered."
             )
 
-        # Logic for creating the shortened URL
-        return {
-            "id": 1,
-            "url": "https://example.com/shortened-url",
-            "shortCode": "abc123",
-            "createdAt": datetime(2023, 1, 1, 0, 0, 0),
-            "updatedAt": datetime(2023, 1, 1, 0, 0, 0),
-        }
+        shortCode = self.generate_short_code()
+
+        data = self.db.save_shorten_url(url, shortCode)
+
+        data["shortCode"] = shortCode
+
+        return data
 
     def get_shorten_url(self, shortCode: str):
-        # Check if shortCode exists
-        if shortCode != "abc123":
-            return HTTPException(
+        data = self.db.get_shorten_url(shortCode)
+
+        # If URL no exists
+        if data is None:
+            raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND, # Not Found
                 detail="Shortened URL not found."
             )   
 
         # Logic for retrieving the shortened URL details
-        return {
-            "id": 1,
-            "url": "https://example.com/shortened-url",
-            "shortCode": f"{shortCode}",
-            "createdAt": datetime(2023, 1, 1, 0, 0, 0),
-            "updatedAt": datetime(2023, 1, 1, 0, 0, 0),
-        }
+        return data
 
     def update_shorten_url(self, shortCode: str, url: HttpUrl):
+        url = str(url)  # Convert HttpUrl to string
+
         # If the shortCode does not exist, return a 404 error
-        if shortCode != "abc123":
-            return HTTPException(
+        if not self.db.short_code_exists(shortCode):
+            raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Shortened URL not found."
             )
 
         # If the URL is already registered, return a 409 error
-        if url == "https://example.com/shortened-url":
-            return HTTPException(
+        if self.db.url_exists(url):
+            raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
                 detail="URL is already registered."
             )
     
         # Logic for updating the shortened URL
-        return {
-            "id": 1,
-            "url": "https://example.com/shortened-url",
-            "shortCode": f"{shortCode}",
-            "createdAt": datetime(2023, 1, 1, 0, 0, 0),
-            "updatedAt": datetime(2023, 1, 1, 0, 0, 0),
-        }
+        return self.db.update_shorten_url(shortCode, url)
 
     def delete_shorten_url(self, shortCode: str):
         # If the shortCode does not exist, return a 404 error
-        if shortCode != "abc123":
-            return HTTPException(
+        if not self.db.short_code_exists(shortCode):
+            raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Shortened URL not found."
             )
 
+        self.db.delete_shorten_url(shortCode)
+
         # Logic for deleting the shortened URL
-        return {
-            "id": 1,
-            "url": "https://example.com/shortened-url",
-            "shortCode": f"{shortCode}",
-            "createdAt": datetime(2023, 1, 1, 0, 0, 0),
-            "updatedAt": datetime(2023, 1, 1, 0, 0, 0),
-        }
+        return None
 
     def get_shorten_url_stats(self, shortCode: str):
         # If the shortCode does not exist, return a 404 error
-        if shortCode != "abc123":
-                    return HTTPException(
-                        status_code=status.HTTP_404_NOT_FOUND,
-                        detail="Shortened URL not found."
-                    )
+        if not self.db.short_code_exists(shortCode):
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Shortened URL not found."
+            )
 
         # Logic for retrieving the shortened URL statistics
-        return {
-            "id": 1,
-            "url": "https://example.com/shortened-url",
-            "shortCode": f"{shortCode}",
-            "createdAt": datetime(2023, 1, 1, 0, 0, 0),
-            "updatedAt": datetime(2023, 1, 1, 0, 0, 0),
-            "clicks": 100,
-        }
+        return self.db.get_shorten_url_stats(shortCode)
+    
+    def generate_short_code(self):
+        import random
+        import string
+
+        chars = string.ascii_letters + string.digits
+        result = ''.join(random.choices(chars, k=6))
+
+        return result
