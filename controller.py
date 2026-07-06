0c31c1905ff0deb945fc8f2230e6ee5467d4d09f
Aki-new <morillomiguel2707@gmail.com>
2026-07-06 01:25:09 -0400

    fix: update database interaction to use cursor and connection parameters

--- controller.py
+++ controller.py
@@ -6,6 +6,7 @@ from model import Database
 class URLController:
     def __init__(self):
         self.db = Database()
+        self.cursor, self.conn = self.db.conectar()
 
     def create_shorten_url(self, url: HttpUrl):
         url = str(url)  # Convert HttpUrl to string
@@ -13,7 +14,7 @@ class URLController:
 
         shortCode = self._generate_short_code()
 
-        data = self.db.save_shorten_url(url, shortCode)
+        data = self.db.save_shorten_url(url, shortCode, self.cursor, self.conn)
         data["shortCode"] = shortCode
 
         return data
@@ -22,7 +23,7 @@ class URLController:
         # If the short code does not exist, neither does the URL.
         self._ensure_short_code_exists(shortCode)
         # Logic for retrieving the shortened URL details
-        return self.db.get_shorten_url(shortCode)
+        return self.db.get_shorten_url(shortCode, self.cursor)
 
     def update_shorten_url(self, shortCode: str, url: HttpUrl):
         url = str(url)  # Convert HttpUrl to string
@@ -31,19 +32,19 @@ class URLController:
         # If the URL is already registered, return a 409 error
         self._ensure_url_not_exists(url)
 
-        return self.db.update_shorten_url(shortCode, url)
+        return self.db.update_shorten_url(shortCode, url, self.cursor, self.conn)
 
     def delete_shorten_url(self, shortCode: str):
         # If the shortCode does not exist, return a 404 error
         self._ensure_short_code_exists(shortCode)
 
-        return self.db.delete_shorten_url(shortCode)
+        return self.db.delete_shorten_url(shortCode, self.cursor, self.conn)
 
     def get_shorten_url_stats(self, shortCode: str):
         # If the shortCode does not exist, return a 404 error
         self._ensure_short_code_exists(shortCode)
 
-        return self.db.get_shorten_url_stats(shortCode)
+        return self.db.get_shorten_url_stats(shortCode, self.cursor)
     
     def _generate_short_code(self):
         """
@@ -63,7 +64,7 @@ class URLController:
         """
             Raise a 404 of the short code does not exist.
         """
-        if not self.db.short_code_exists(shortCode):
+        if not self.db.short_code_exists(shortCode, self.cursor):
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Shortened URL not found."
@@ -73,7 +74,7 @@ class URLController:
         """
             Raise a 409 error if the URL already exists.
         """
-        if self.db.url_exists(url):
+        if self.db.url_exists(url, self.cursor):
             raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
                 detail="URL is already registered."
--- model.py
+++ model.py
@@ -2,20 +2,22 @@ import sqlite3 as db
 from datetime import datetime
 
 class Database:
-    def __init__(self):
-        self.connection = db.connect("database/url_shortener.db")
-        self.cursor = self.connection.cursor()
+    def conectar(self, path: str, name: str):
+        connection = db.connect(f"{path}/{name}.db")
+        cursor = connection.cursor()
 
-    def save_shorten_url(self, url: str, shortCode: str):
+        return cursor, connection
+
+    def save_shorten_url(self, url: str, shortCode: str, cursor, connection):
         """
             Save a URL to the database
         """
         # Logic for saving the shortened URL to the database
-        self.cursor.execute(
+        cursor.execute(
             "INSERT INTO urls (url, short_code) VALUES (?, ?)",
             (url, shortCode)
         )
-        self.connection.commit()
+        connection.commit()
         return {
             "id": self.cursor.lastrowid,
             "url": url,
@@ -24,12 +26,12 @@ class Database:
             "updatedAt": datetime.now(),
         }
     
-    def get_shorten_url(self, shortCode: str):
+    def get_shorten_url(self, shortCode: str, cursor):
         """
             Get a URL using the shortcode from the database
         """
         # Logic for retrieving the shortened URL details from the database
-        self.cursor.execute(
+        cursor.execute(
             "SELECT url_id, url, short_code, created_at, updated_at FROM urls WHERE short_code = ?",
             (shortCode,)
         )
@@ -46,44 +48,44 @@ class Database:
             "updatedAt": result[4],
         }
 
-    def update_shorten_url(self, shortCode: str, url: str):
+    def update_shorten_url(self, shortCode: str, url: str, cursor, connection):
         """
             Updates a URL associated with a shortcode in the database
         """
         # Logic for updating the shortened URL in the database
-        self.cursor.execute(
+        cursor.execute(
             "UPDATE urls SET url = ? WHERE short_code = ?",
             (url, shortCode)
         )
 
-        self.connection.commit()
+        connection.commit()
 
         return self.get_shorten_url(shortCode)
 
-    def delete_shorten_url(self, shortCode: str):
+    def delete_shorten_url(self, shortCode: str, cursor, connection):
         """
             Deletes a shortcode and its associated URL from the database
         """
         # Logic for deleting the shortened URL from the database
-        self.cursor.execute(
+        cursor.execute(
             "DELETE FROM urls WHERE short_code = ?",
             (shortCode,)
         )
-        self.connection.commit()
+        connection.commit()
 
         return True
     
-    def get_shorten_url_stats(self, shortCode: str):
+    def get_shorten_url_stats(self, shortCode: str, cursor):
         """
             Retrieves statistics for a short code
         """
         # Logic for retrieving the statistics of the shortened URL from the database
-        self.cursor.execute(
+        cursor.execute(
             "SELECT * FROM urls WHERE short_code = ?",
             (shortCode,)
         )
 
-        result = self.cursor.fetchone()
+        result = cursor.fetchone()
         if result is None:
             return None
         
@@ -96,13 +98,13 @@ class Database:
             "clicks": result[5]
         }
     
-    def url_exists(self, url: str):
+    def url_exists(self, url: str, cursor):
         """
             Checks if a URL exists.
             Returns True if it exists.
             Returns False if it does not exist.
         """
-        self.cursor.execute(
+        cursor.execute(
             "SELECT COUNT(*) FROM urls WHERE url = ? LIMIT 1",
             (url,)    
         )
@@ -116,13 +118,13 @@ class Database:
         
         return True
 
-    def short_code_exists(self, shortCode: str):
+    def short_code_exists(self, shortCode: str, cursor):
         """
             Checks if a Short code exists.
             Returns True if it exists.
             Returns False if it does not exist.
         """
-        self.cursor.execute(
+        cursor.execute(
             "SELECT COUNT(*) FROM urls WHERE short_code = ? LIMIT 1",
             (shortCode,)    
         )
@@ -136,5 +138,5 @@ class Database:
 
         return True
 
-    def close(self):
-        self.connection.close()
+    def close(self, connection):
+        connection.close()
